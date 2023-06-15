b872f9f73eb5afc94b66616be7e15b0a0634b0a6
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 20:46:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Good Luck 💪🏾
+function sumTwoSmallestNums(arr) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  var sum = arr[0] + arr[1];
+  return sum;
+}
+console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])); // Output: 17
+console.log(sumTwoSmallestNums([190, 300, 230, 800])); // Output: 420
